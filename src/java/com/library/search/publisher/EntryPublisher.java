273b74cbc72ccb3e4427931606fda7d6e39f2dce
Lucas Napoli <lucas.napoli@gmail.com>
2019-07-26 16:49:07 -0400

    URL as Requested

--- src/java/com/library/search/publisher/EntryPublisher.java
+++ src/java/com/library/search/publisher/EntryPublisher.java
@@ -4,6 +4,6 @@ import javax.xml.ws.Endpoint;
 
 public class EntryPublisher {
     public static void main(String[] args) {
-        Endpoint.publish("http://localhost:9595/LibrarySearchPublisher_war/", new PublisherImp());
+        Endpoint.publish("http://localhost:8092/publishers/", new PublisherImp());
     }
 }
