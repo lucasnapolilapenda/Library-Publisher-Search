8beac7fec65a2ed4b07ec9e4ee73822f20b648bb
Lucas Napoli <lucas.napoli@gmail.com>
2019-07-31 18:10:34 -0400

    Authentication Completed

--- src/java/com/library/search/publisher/PublisherImp.java
+++ src/java/com/library/search/publisher/PublisherImp.java
@@ -9,10 +9,16 @@ package com.library.search.publisher;
 
 
 
+import javax.annotation.Resource;
 import javax.jws.WebService;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
+import javax.xml.ws.WebServiceContext;
+import javax.xml.ws.handler.MessageContext;
+import javax.xml.ws.http.HTTPException;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Represents all services implementation
@@ -21,8 +27,9 @@ import java.util.ArrayList;
 
 public class PublisherImp implements PublisherInterface {
 
-    @Context
-    private UriInfo context;
+    @Resource
+    WebServiceContext context;
+
 
     /**
      * @return returns java.lang.String
@@ -30,6 +37,21 @@ public class PublisherImp implements PublisherInterface {
      */
     @Override
     public String getPublisherInfo(Publisher publisher) throws Exception {
+        if (isAuthenticated()) {
             return new PublisherRepository().searchPublisher(publisher);
+       } else {
+            throw new HTTPException(401);
+       }
+    }
+
+    private boolean isAuthenticated() {
+        MessageContext messageContext = context.getMessageContext();
+        Map httpHeaders = (Map) messageContext.get(MessageContext.HTTP_REQUEST_HEADERS);
+        List userNameList = (List) httpHeaders.get("username");
+        List passwordList = (List) httpHeaders.get("password");
+        if (userNameList.contains("book-service") && passwordList.contains("book-service-secure-password-1234"))
+        return true;
+        else
+        return false;
     }
 }
